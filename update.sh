785600c4f11ed16f3e3fff70b62c51f9a174c2e5
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-09-09 16:37:06 +0200

    deploy with ansible

--- update.sh
+++ update.sh
@@ -3,26 +3,8 @@ set -eEuo pipefail
 set -x
 
 main() {
-    export host="$1"
-    export hostname="$host"
-    export conn="$host"
-
-    scp ./remote-*.sh ./configuration.nix "$conn:~"
-    ssh "$conn" ./remote-cache.sh "$hostname"
-    ssh "$conn" ./remote-configuration.sh "$hostname"
-
-    echo "Comparing with existing config:"
-    if ssh "$conn" diff --color -C 3 /etc/nixos/configuration.nix ./configuration.nix ; then
-        return 0
-    fi
-
-    read -p "Accept and execute nixos-rebuild? y/n : " yn
-    [[ "$yn" != "y" ]] && return 0
-
-    ssh "$conn" sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-    ssh "$conn" sudo nix-channel --update
-    ssh "$conn" sudo cp ./configuration.nix /etc/nixos/
-    ssh "$conn" sudo nixos-rebuild switch
+    . .venv/bin/activate
+    ansible-playbook ansible/deploy.yml
 }
 
 main "$@"
